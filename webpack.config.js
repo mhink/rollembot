aa9ac8b364aca1bca23c065f854ffab414f2da5c
Matt Hink <mhink1103@gmail.com>
2016-08-04 15:11:33 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const webpack = require('webpack')
+const fetch = require('node-fetch')
+const { map, fromPairs } = require('lodash')
+const paths = {
+  source:   path.resolve('src'),
+  output:   path.resolve('dist'),
+  modules:  path.resolve('node_modules')
+}
+const pkg = require('./package.json')
+
+const additionalModules = {
+  'auth0-api-jwt-rsa-validation': true,
+  'auth0-authz-rules-api': true,
+  'auth0-oauth2-express': true,
+  'auth0-sandbox-ext': true,
+  'detective': true,
+  'sandboxjs': true,
+  'webtask-tools': true
+}
+
+const fetchModules = fetch(pkg.webtaskDependenciesUrl)
+  .then(res     => res.json())
+  .then(json    => json.modules)
+  .then(modules => fromPairs(map(modules, module => [module.name, true])))
+  .then(modules => Object.assign({}, modules, additionalModules))
+
+const webpackConfig = fetchModules.then(externals => ({
+  target: 'node',
+  context: paths.source,
+  node: false,
+
+  entry: {
+    [pkg.name]: './main.js'
+  },
+  externals, // result of fetchModules promise
+
+  output: {
+    path:          paths.output,
+    filename:      "[name].js",
+    library:       true,
+    libraryTarget: 'commonjs2'
+  },
+
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+  ],
+
+  resolve: {
+    modulesDirectories: ['src', 'node_modules'],
+    extensions: ['', '.js']
+  }
+}))
+
+module.exports = webpackConfig
